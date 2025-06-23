3ca2443b66627b4bd5e7e8629c06a3d829fbe0ab
hasuburero <hasuburero0802@gmail.com>
2025-06-23 09:53:50 +0900

    NewSession is OK

--- lib/controller/api/session/session.go
+++ lib/controller/api/session/session.go
@@ -2,7 +2,7 @@ package session
 
 import (
 	"errors"
-	"net/http"
+	"strconv"
 )
 
 import (
@@ -13,7 +13,7 @@ import (
 type Session struct {
 	Hosts  map[string]*Host
 	Groups map[string]Group
-	Tid    int
+	Tid    int64
 }
 
 type Host struct {
@@ -35,11 +35,18 @@ type Group struct {
 
 // section2
 const (
-	TransactionIdSize = 32 //32 characters
+	TransactionIdSize = 16 //16 characters
+	Base              = 10
 )
 
-func (self *Session) NewTid() (string, error) {
+func (self *Session) NewTid() string {
+	tid := strconv.FormatInt(self.Tid, Base)
+	zero := ""
+	for i := 0; i < TransactionIdSize-len(tid); i++ {
+		zero += "0"
+	}
 
+	return zero + tid
 }
 
 func NewSession(filename string) (*Session, error) {
@@ -49,6 +56,7 @@ func NewSession(filename string) (*Session, error) {
 	}
 
 	var new_session = new(Session)
+	new_session.Hosts = make(map[string]*Host)
 	new_session.Groups = make(map[string]Group)
 	for _, ctx := range config.Node {
 		var new_host = new(Host)
@@ -67,13 +75,15 @@ func NewSession(filename string) (*Session, error) {
 		}
 
 		var node_buf = make(map[string]*Host)
+		var node_list = make([]*Host, 0)
 		for _, node := range ctx.Nodename {
 			if _, exists := node_buf[node]; exists {
 				return nil, errors.New("NodeName is already exists, inside the Group field\n")
 			}
 			node_buf[node] = new_session.Hosts[node]
+			node_list = append(node_list, new_session.Hosts[node])
 		}
-		new_session.Groups[ctx.Name]
+		new_session.Groups[ctx.Name] = Group{node_list}
 	}
 	return new_session, nil
 }
