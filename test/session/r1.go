7c95394e5992a4890c2527e80597318dcdad6cac
hasuburero <hasuburero0802@gmail.com>
2025-06-23 09:52:28 +0900

    adding test. NewSession() is OK

---
+++ test/session/r1.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"github.com/hasuburero/ReeX/lib/controller/api/session"
+)
+
+const (
+	filename = "session.json"
+)
+
+func main() {
+	Session, err := session.NewSession(filename)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for _, ctx := range Session.Hosts {
+		fmt.Println(ctx)
+	}
+
+	fmt.Println("")
+	for key, ctx := range Session.Groups {
+		fmt.Println(key, ctx)
+	}
+
+	return
+}
